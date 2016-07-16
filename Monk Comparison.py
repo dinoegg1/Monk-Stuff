82d19fc3c4d6a79419dc29bb23cf03c6dbc49751
dinoegg1 <rachelmarielizabeth@yahoo.com>
2016-07-16 00:03:25 -0400

    Add files via upload

--- Monk Comparison.py
+++ Monk Comparison.py
@@ -1,32 +1,19 @@
-spell = input("Please Enter your Spell Power rating: ")
+print ("Please Enter your Spell Power rating")
+spell = float (input())
 def Essence(a):
 	if a == 0:
 		return 'Fail'
 	else:
 		return ((1.10 * a) + (0.36 * a)) * 6	
-
-		
-		
-		
 		
 def Vivify (b):
 	if b == 0:
 		return 'Fail'
 	else:
 		return ((b * 2.75) * 3)
-		
-	
+				
+y = Essence(spell) - Vivify(spell)
+print(y)
 
 	
-		
-def Comparison (c):
-	if c == 0:
-		return 'Fail'
-	else:
-		return (Essence (c) - Vivify (c))
-
-		
-		
-		
-		
 input('Press ENTER to exit')
