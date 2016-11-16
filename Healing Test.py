dc59b49f67a662c8002a6d2a81e9d080f844a7ee
dinoegg1 <rachelmarielizabeth@yahoo.com>
2016-11-16 04:00:54 -0500

    Add files via upload

--- Healing Test.py
+++ Healing Test.py
@@ -1,48 +1,44 @@
-#saving for imports
 import random
-print ("Enter the amount of players in your raid")
-raiders = float ( input ()) 
-#this is the idiot factor
-def fire_standing (a):
-	if a == 0:
-		return 'Invaild Data, Restart'
-				#debugging
-	else:
-		return a * 0.25
-		#0.25 is the factor ratio between amount of players taking fire damage and 20 man raid
-z = fire_standing (raiders)
-print (z)
-print ("This is the average amount of players that will stand in fire")
-def Distance_Factor (b):
-	return ( random.randint(0, 10) + b ) / 10
-#Artillery
-y = Distance_Factor (0) * z * 210000
-#Blitz & Dot
-x = (z * 70000) + (160500 * z)
-#Falling Slam
-w = 45000
-#Extra Falling Slam
-v = 300000 * z
-def Fire_Bomb (b):
-	return b * random.randint (0, 4)
-	#Add independent rolls for each bomb max 4
-FireBomb = Fire_Bomb (142857)
-def Immolation (a):
-	return a * random.randint (0, 9)
-	#Add independent rolls for each Fire max 9
-i = Immolation (raiders)
-def Pounding (a):
-	return a * 91800
-	
-Pound = Pounding (raiders)
-def Unstable_Orb (a):
-	return a * random.randint(0, 9)
-	#Add independent rolls max 9
-Orb = Unstable_Orb (raiders)
-Barrage (a):
-	return 190000 * a
-	
-u = Barrage (raiders)
-#Air Phase: y, w, v, FireBomb, i
-#Ground Phase: x, u, Orb, i, Pound
-#1 cycle = 200 seconds
+import math
+print ("Enter the number of players in your raid")
+raiders = float (input())
+if raiders > 30:
+    print('Error, Try again.')
+else:      
+    def fire_standing (a):
+        if a == 0:
+            return 'Invaild Data, Restart'
+                    #debugging
+        if a == 1:
+          return a * 0.25
+        else:
+            return a / (0.0125 * math.pow((a-0), 2) + 0 )
+            #0.25 is the factor ratio between amount of players taking fire damage and 20 man raid
+    
+    def Distance_Factor (b):
+        if b >= 1:
+            return 'Error'
+        else:
+            return (random.randint(0, 10) + b ) / 10
+    
+    def Fire_Bomb (c):
+        return (c * random.random()) + (c * random.random()) + (c * random.random()) + (c * random.random())
+    
+    def Immolation (d):
+        return d * random.randint (0, 9)
+    
+    def Pounding (e):
+        return e * 91800
+    
+    def Barrage (f):
+        return 190000 * f
+    if raiders == 20:    
+        y = Distance_Factor (0) + fire_standing (1) + Fire_Bomb (1) + Immolation (1) + Pounding (1) + Barrage (1)
+        z = "Is the estimated damage your raid will take"
+        print (round(y, 0), z)
+    else:
+        h = Distance_Factor (0) + fire_standing (raiders) + Fire_Bomb (1) + Immolation (1) + Pounding (1) + Barrage (1)
+        i = "Is the estimated damage your raid will take"
+        print (round(h, 0), i)
+    #don't do this, this is very painful to debug
+input('Press ENTER to exit')
